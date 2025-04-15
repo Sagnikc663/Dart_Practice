c8a08791be1a05a049075d8425dff6b7b8d5ccfc
Sagnik Chakraborty <65452981+Sagnikc663@users.noreply.github.com>
2025-04-16 00:20:08 +0530

    Time: 59 ms (10.53%) | Memory: 193 MB (5.27%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.dart
@@ -0,0 +1,18 @@
+class Solution {
+  bool containsDuplicate(List<int> nums) {
+    Map<int, int> map= {};
+
+
+    bool ans = false;
+    for(int i=0;i<nums.length;i++){
+        if(map.containsKey(nums[i])){
+            ans = true;
+            map[nums[i]] = (map?[nums[i]] ?? 0) + 1;
+        }else{
+            map[nums[i]] = 0;
+        }
+    }
+
+    return ans;
+  }
+}
