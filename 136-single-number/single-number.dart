45b79ddbb2413095b787277c767133e1d7668075
Sagnik Chakraborty <65452981+Sagnikc663@users.noreply.github.com>
2025-04-26 00:39:14 +0530

    Time: 0 ms (100.00%) | Memory: 150 MB (71.69%) - LeetSync

--- 136-single-number/single-number.dart
+++ 136-single-number/single-number.dart
@@ -1,9 +1,9 @@
 class Solution {
   int singleNumber(List<int> nums) {
-    int ans = 0;
-    for(final i in nums){
-        ans = i ^ ans;
-    }
-    return ans;
+   int result = 0;
+   for(final i in nums){
+    result = result ^ i;
+   }
+   return result;
   }
 }
