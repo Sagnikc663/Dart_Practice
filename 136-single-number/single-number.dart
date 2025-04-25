b56d97a0db602a30080403d46e3fe1a550a67ce6
Sagnik Chakraborty <65452981+Sagnikc663@users.noreply.github.com>
2025-04-26 00:39:53 +0530

    Time: 1 ms (66.87%) | Memory: 148.6 MB (95.78%) - LeetSync

--- 136-single-number/single-number.dart
+++ 136-single-number/single-number.dart
@@ -1,5 +1,6 @@
 class Solution {
   int singleNumber(List<int> nums) {
+    ///result ^ i (eg: 0 ^ 2 = 2; 2^2= 0 ; 0^1=1; ans = 1)
    int result = 0;
    for(final i in nums){
     result = result ^ i;
