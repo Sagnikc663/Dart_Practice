f738ac40da9f568e839f2f6d66ca9a1d54a360af
Sagnik Chakraborty <65452981+Sagnikc663@users.noreply.github.com>
2025-05-17 15:41:24 +0530

    Time: 0 ms (100.00%) | Memory: 150.9 MB (23.53%) - LeetSync

---
+++ 75-sort-colors/sort-colors.dart
@@ -0,0 +1,32 @@
+class Solution {
+  void sortColors(List<int> nums) {
+    int low =0,mid=0,high=nums.length-1;
+
+    while(mid<= high){
+        switch(nums[mid]){
+            case 0:
+            {
+                final temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+                break;
+            }
+            case 1:
+            {
+                mid++;
+                break;
+            }
+            case 2:
+            {
+                final temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+                break;
+            }
+        }
+    }
+  }
+}
