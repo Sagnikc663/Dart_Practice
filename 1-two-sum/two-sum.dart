643e1714268e5e02e4b9ef07f3d813514fb0d722
Sagnik Chakraborty <65452981+Sagnikc663@users.noreply.github.com>
2026-05-23 12:42:55 +0530

    Time: 9 ms (66.43%) | Memory: 147.9 MB (84.48%) - LeetSync

--- 1-two-sum/two-sum.dart
+++ 1-two-sum/two-sum.dart
@@ -1,25 +1,16 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-    //BruteForce and Optimal
-     List<int> ans= [];
-    // for(int i=0;i<nums.length;i++){
-    //     for(int j=i+1;j<nums.length;j++){
-    //         if(nums[i] + nums[j] == target){
-    //             ans.add(i);
-    //             ans.add(j);        
-    //             }
-    //     }
-    // }
+    Map<int,int> map = {};
 
-   for(int i=0;i<nums.length;i++){
-    final tElement = target - nums[i];
-    final index = nums.indexOf(tElement,i+1);
-    if(index != -1 && (nums[index] + nums[i] == target)){
-       ans.add(i);
-       ans.add(index);
+    for(int i=0;i<nums.length;i++){
+        int key = target - nums[i];
+        if(map.containsKey(key)){
+            return [map[key] ?? 0,i];
+        }
+
+        map[nums[i]] = i;
     }
-   }
 
-    return ans;
+    return [];
   }
 }
